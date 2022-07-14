a52dc4e10325e2cf1d1db7d9c0628ae3717373b4
DmytroMelnychenko <meldimld@gmail.com>
2022-07-14 13:09:14 +0300

    qw

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import React, {Component} from 'react';
 
+import {Posts} from "./components/posts/Posts";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+              <Posts/>
+            </div>
+        );
+    }
+}
+
+export default App;
